feffc8c517ae79cefb1f511ce5f8e5d09f6b1773
Youssef <allamyoussef360@gmail.com>
2025-02-07 21:21:26 +0200

    make ui for sign in and sign up

---
+++ app/src/main/java/com/youssef/foodplanner/Login.java
@@ -0,0 +1,39 @@
+package com.youssef.foodplanner;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Login extends Fragment {
+
+
+    public Login() {
+        // Required empty public constructor
+    }
+
+
+
+//    public static Login newInstance(String param1, String param2) {
+//        Login fragment = new Login();
+//        Bundle args = new Bundle();
+//
+//    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
